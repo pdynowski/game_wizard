67b5b9191f11db25cf915de281ec0ef1963c1b46
Paul Dynowski <paul.dynowski@gmail.com>
2019-06-13 00:53:48 -0500

    basic editing for game model

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,4 +8,18 @@ class GamesController < ApplicationController
 		@game = Game.find(params[:id])
 	end
 
+	def edit
+		@game = Game.find(params[:id])
+	end
+
+	def update
+		game = Game.find(params[:id])
+		game.update(allowed_params)
+		redirect_to game_path(game)
+	end
+
+	def allowed_params
+		params.require(:game).permit(:title, :min_players, :max_players, :description, :external_link)
+	end
+
 end

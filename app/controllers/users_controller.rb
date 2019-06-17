a5d8f395b9a63de61bc7c84483c00d85009a818f
Paul Dynowski <paul.dynowski@gmail.com>
2019-06-16 20:52:04 -0500

    add base users and sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+
+	
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def index
+	end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_021806) do
+ActiveRecord::Schema.define(version: 2019_06_16_231659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2019_06_13_021806) do
     t.index ["max_players"], name: "index_games_on_max_players"
     t.index ["min_players"], name: "index_games_on_min_players"
     t.index ["title"], name: "index_games_on_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "display_name"
+    t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end

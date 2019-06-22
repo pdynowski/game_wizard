35b8fd0e584c6193c95917fe7fd6993a4eeb142c
Paul Dynowski <paul.dynowski@gmail.com>
2019-06-21 19:11:26 -0500

    play with styling for login page and forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,15 @@ class SessionsController < ApplicationController
 
 	def new
 		if current_user
-			redirect_to user_path(current_user) 
+			redirect_to user_path(current_user)
 		else
 			render :new, locals: {error: ""}
 		end
 	end
 
 	def create
-		user = User.find_by_email(params[:email])
-		if user && user.authenticate(params[:password])
+		user = User.find_by_email(params[:login][:email])
+		if user && user.authenticate(params[:login][:password])
 			session[:user_id] = user.id
 			redirect_to user_path(user)
 		else

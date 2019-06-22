6cbef309f66532ed8f0adc23fba32a7fc2b491da
Paul Dynowski <paul.dynowski@gmail.com>
2019-06-22 15:25:52 -0500

    styling changes - moved some classes around, mostly styling for user create form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
 
   def remove_user_header?
     return true if ["sessions"].include? params["controller"]
-    return true if ["users"].include?(params["controller"]) && ["new"].include?(params["action"])
+    return true if ["users"].include?(params["controller"]) && ["new", "create"].include?(params["action"])
   end
 end

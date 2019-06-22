92c9499967f613e59271c5a7f4809014aae042e1
Paul Dynowski <paul.dynowski@gmail.com>
2019-06-22 14:49:26 -0500

    remove debugger

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
 
   def remove_user_header?
     return true if ["sessions"].include? params["controller"]
-    debugger
     return true if ["users"].include?(params["controller"]) && ["new"].include?(params["action"])
   end
 end
